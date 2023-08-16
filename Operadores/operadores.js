611da7af0cc0bf38617d0db7f0647f531e0b396b
mel-teodosio <134503590+mel-teodosio@users.noreply.github.com>
2023-08-16 09:56:04 -0300

    Add files via upload

---
+++ Operadores/operadores.js
@@ -0,0 +1,39 @@
+    var Val1 = document.getElementById('Valor1');
+    var Val2 = document.getElementById('Valor2');
+
+    function SomaValores(){
+        var s1 = document.getElementById("valor1").value;
+        var s2 = document.getElementById("valor2").value;
+        var s3 = parseInt(s1) + parseInt(s2);
+        alert(s3);
+
+       
+    }
+
+    function SubtraçãoValores(){
+        var s1 = document.getElementById("valor1").value;
+        var s2 = document.getElementById("valor2").value;
+        var s3 = parseInt(s1) - parseInt(s2);
+        alert(s3);
+
+       
+    }
+
+    function MultiplicaçãoValores(){
+        var s1 = document.getElementById("valor1").value;
+        var s2 = document.getElementById("valor2").value;
+        var s3 = parseInt(s1) * parseInt(s2);
+        alert(s3);
+
+       
+    }
+
+    function DivisãoValores(){
+        var s1 = document.getElementById("valor1").value;
+        var s2 = document.getElementById("valor2").value;
+        var s3 = parseInt(s1) / parseInt(s2);
+        alert(s3);
+
+       
+    }
+
